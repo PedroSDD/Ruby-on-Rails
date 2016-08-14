dea2ca52fdd4db7978effbeef4dcc3fa853314fb
PedroSDD <psdd91@gmail.com>
2016-08-14 20:12:58 +0100

    Functional Tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get index" do                                 #This is an example of a functiona test
+    get :index                                               #This kind of test verify if that the model, view and controller work well
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
