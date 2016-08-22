57faa260e12cde9e316e73732df978b013ebe20f
PedroSDD <psdd91@gmail.com>
2016-08-22 17:14:17 +0100

    Moving the cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)     # @ sign means that we are dealing with an object of the class
   end                                     #Notice that by doing order(:title) we are ordering by alphabetical order
